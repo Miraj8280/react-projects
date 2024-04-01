3dd4cc51a5d4f476a3c12446c4170040d3947622
Miraj Asraf <mirajasraf786@gmail.com>
2024-04-02 05:04:34 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import "./App.css";
-import { ContactForm } from "./components/EventHandling";
 // import MultiState from './components/MultiState';
 // import User from './components/User';
 // import Counter from './components/Counter';
 // import List from "./components/List";
 // import Greeting from "./components/Greeting";
 // import Form from "./components/Form";
-// import ParentComponent from "./components/ParentComponent";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div>
-      <ContactForm />
+      <ParentComponent />
     </div>
   );
 }
