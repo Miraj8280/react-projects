4d5a2fbeef4b433c9f8736ea72c4ea55aeb7f982
Miraj Asraf <mirajasraf786@gmail.com>
2024-05-08 23:50:57 +0530

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Accordion from "./components/01_accordion/Accordion";
 import ColorGenerator from "./components/02_color-generator/ColorGenerator";
 import StarRating from "./components/03_star-rating/StarRating";
 import ImageSlider from "./components/04_image-slider/ImageSlider";
+import Footer from "./components/05_footer/Footer";
 
 function App() {
   return (
@@ -17,6 +18,10 @@ function App() {
 
       {/* Image Slider */}
       <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
+
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/05_footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <h3>
+        Made with <span className="heart">❤</span> by{" "}
+        <a href="https://www.linkedin.com/in/miraj-asraf/" target="blank" className="myName">
+          Miraj Asraf
+        </a>
+      </h3>
+    </div>
+  );
+}
